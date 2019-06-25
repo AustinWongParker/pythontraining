ed61dd5ad8ca0f4f257201f751b4c79e5b8c1e86
Austin Wong <wongparker97@gmail.com>
2019-06-24 20:02:04 -0500

    example program for pygame

--- mygame.py
+++ mygame.py
@@ -5,18 +5,31 @@ Austin Wong-Parker
 
 6 - 11 - 19
 
-** All code is written by me for PRACTICE only **
+** Taken from https://www.pygame.org/docs/tut/PygameIntro.html **
 '''
 
-import random, os.path
-import pygame
-from pygame.locals import *
-from src.Game import Game
+import sys, pygame
+pygame.init()
 
-def main():
-    game = Game()
-    game.loop()
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
 
+screen = pygame.display.set_mode(size)
 
-if __name__ == '__main__':
-    main()
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
